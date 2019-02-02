e28f896f57f1084ffb30e60bed845fb567da7f3a
diiir727 <diiir727@gmail.com>
2019-02-02 17:59:56 +0300

    refactoring and docs
    * add readme
    * add usage

--- src/main/java/evservice/RatPackServer.java
+++ src/main/java/evservice/RatPackServer.java
@@ -7,19 +7,30 @@ import ratpack.server.RatpackServer;
 public class RatPackServer {
 
     public static void main(String[] args) throws Exception {
-//        String host = "jdbc:postgresql://localhost:5432/den";
-//        String login = "den";
-//        String password = "123";
-        if (args.length != 3)
-            throw new IllegalArgumentException("not valid args");
+        if (args.length != 3) {
+            printHelp();
+        } else {
+            run(args);
+        }
+    }
 
-        String jdbcUrl = args[0];
-        String login = args[1];
-        String password = args[2];
+    private static void run(String[] args){
+        try {
+            String jdbcUrl = args[0];
+            String login = args[1];
+            String password = args[2];
 
-        DAO dao = new PostgresDAO(jdbcUrl, login, password);
-        RatpackServer.start(server -> server
-                .handlers(chain -> chain
-                        .post(new PostHandler(dao))));
+            DAO dao = new PostgresDAO(jdbcUrl, login, password);
+            RatpackServer.start(server -> server
+                    .handlers(chain -> chain
+                            .post(new PostHandler(dao))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printHelp(){
+        System.out.println("Not valid params.");
+        System.out.println("usage: <jdbcUrl>, <login>, <password>");
     }
 }
--- src/main/java/evservice/workers/Worker.java
+++ src/main/java/evservice/workers/Worker.java
@@ -3,14 +3,14 @@ package evservice.workers;
 import com.fasterxml.jackson.databind.JsonNode;
 import evservice.core.User;
 
-abstract class Worker {
+public abstract class Worker {
 
     /**
      * Обрабатывает входные параметры и выдает необходимый результат
      * @param request входные параметры в формате JSON
      * @return результат в формате JSON
      */
-    abstract JsonNode getResult(JsonNode request);
+    public abstract JsonNode getResult(JsonNode request);
 
     User getUserFromRequest(JsonNode request) {
         String login = request.get("login").asText("");
