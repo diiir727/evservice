575993e08cc05451dc52e01bc774f3b004acc650
diiir727 <diiir727@gmail.com>
2019-02-02 14:33:46 +0300

    test refactoring

--- src/test/java/evservice/workers/GetBalanceWorkerTest.java
+++ src/test/java/evservice/workers/GetBalanceWorkerTest.java
@@ -13,64 +13,47 @@ import static org.mockito.Mockito.when;
 
 public class GetBalanceWorkerTest extends TestCase {
     private DAO dao = mock(DAO.class);
-    private ObjectMapper mapper= new ObjectMapper();
+    private ObjectMapper mapper = new ObjectMapper();
+    private GetBalanceWorker worker = new GetBalanceWorker(dao);
+    private User testUser = new User(0, "test", "test");
 
     public void testSuccessCase() throws SQLException {
-        ObjectNode obj = mapper.createObjectNode();
-        obj.put("login", "test");
-        obj.put("password", "test");
-        GetBalanceWorker worker = new GetBalanceWorker(dao);
-        User user = new User(0, "test", "test");
-        when(dao.getUserByLogin(user)).thenReturn(user);
-        when(dao.getBalance(user)).thenReturn(12.5);
-
+        ObjectNode obj = createRequestJson("test");
+        when(dao.getUserByLogin(testUser)).thenReturn(testUser);
+        when(dao.getBalance(testUser)).thenReturn(12.5);
         assertEquals(worker.getResult(obj).get("extras").get("balance").asDouble(), 12.5);
     }
 
     public void testUserNotExist() throws SQLException {
-        GetBalanceWorker worker = new GetBalanceWorker(dao);
-        ObjectNode obj = mapper.createObjectNode();
-
-        User user = new User(0, "test", "test");
-        when(dao.getUserByLogin(user)).thenReturn(null);
-        obj.put("login", "test");
-        obj.put("password", "test");
-
+        ObjectNode obj = createRequestJson("test");
+        when(dao.getUserByLogin(testUser)).thenReturn(null);
         assertEquals(worker.getResult(obj).get("result").asInt(), GetBalanceWorker.USER_NOT_EXIST);
     }
 
     public void testNotValidArgs() throws SQLException {
         GetBalanceWorker worker = new GetBalanceWorker(dao);
-        ObjectNode obj = mapper.createObjectNode();
-
-        obj.put("login", "");
-        obj.put("password", "test");
-
+        ObjectNode obj = createRequestJson("");
         assertEquals(worker.getResult(obj).get("result").asInt(), GetBalanceWorker.SOME_ERROR);
     }
 
     public void testSomeException() throws SQLException {
-        GetBalanceWorker worker = new GetBalanceWorker(dao);
-        ObjectNode obj = mapper.createObjectNode();
-
-        User user = new User(0, "test", "test");
-        when(dao.getUserByLogin(user)).thenThrow(SQLException.class);
-        obj.put("login", "test");
-        obj.put("password", "test");
-
+        ObjectNode obj = createRequestJson("test");
+        when(dao.getUserByLogin(testUser)).thenThrow(SQLException.class);
         assertEquals(worker.getResult(obj).get("result").asInt(), GetBalanceWorker.SOME_ERROR);
     }
 
     public void testPasswordNotValid() throws SQLException {
-        GetBalanceWorker worker = new GetBalanceWorker(dao);
-        ObjectNode obj = mapper.createObjectNode();
-
+        ObjectNode obj = createRequestJson("test");
         User user = new User(0, "test", "other_pass");
         when(dao.getUserByLogin(user)).thenReturn(user);
-        obj.put("login", "test");
-        obj.put("password", "test");
-
         assertEquals(worker.getResult(obj).get("result").asInt(), GetBalanceWorker.PASSWORD_FAIL);
+    }
+
+    private ObjectNode createRequestJson(String login){
+        ObjectNode obj = mapper.createObjectNode();
+        obj.put("login", login);
+        obj.put("password", "test");
+        return obj;
     }
 
 }
